0f816abc5b71980a37ad1eff011d3e3b298fcfff
Jan Sodomka <h.sodomka@seznam.cz>
2026-06-29 18:54:30 +0200

    force fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
+
+  fonts: [
+    {
+      provider: fontProviders.fontshare(),
+      name: 'Satoshi',
+      cssVariable: '--font-body',
+      weights: [300, 400, 500],
+      styles: ['normal'],
+    },
+  ],
 
   vite: {
     plugins: [tailwindcss()],
